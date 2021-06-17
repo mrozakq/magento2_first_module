2088d6bc42652962449585f061494f5050845e3c
mrozakq <76102342+mrozakq@users.noreply.github.com>
2021-06-17 09:29:33 +0200

    Add files via upload
    My awesome magento 2 module

---
+++ app/code/mrozaq/regmodule/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponenetRegistrar::MODULE,
+        'mrozaq_Magento2Module',
+        __DIR__
+);
